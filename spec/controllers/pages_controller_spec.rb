fa74cb89b660e113f4c9528c440ecb7eba2f6539
Derek Monti <d.monti108@gmail.com>
2011-11-19 19:32:03 -0700

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,7 +6,8 @@ describe PagesController do
   before(:each) do
     
     @base_title = "Ruby on Rails Tutorial Sample App"
-end
+  
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
